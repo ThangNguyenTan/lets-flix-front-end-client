8f91f46677e8e5e4a1ee4cbef98f1f810706c59d
ThangNguyenTan <85928224+ThangNguyenTan@users.noreply.github.com>
2023-02-18 14:36:31 +0700

    fix: update backend api

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 //export const MAIN_PROXY_URL = `http://localhost:5000`;
-export const MAIN_PROXY_URL = `https://node-js-movie-database-season.herokuapp.com`;
+export const MAIN_PROXY_URL = `https://lets-flix-back-end.vercel.app/`;
 export const customerID = `5f700f89bf9e1f32f4e8b617`;
